a53e233f66b51d32ff069eb8627198a90516dc9a
AbobaShlepa <nicefsf@icloud.com>
2024-05-02 09:29:48 +0300

    Fixed arrows

--- src/helpers/arrow.ts
+++ src/helpers/arrow.ts
@@ -1,17 +1,18 @@
-import arrowLine from 'arrow-line'
-import useTalentStore from '@/state/TalentsStore'
+import arrowLine from 'arrow-line';
+import useTalentStore from '@/state/TalentsStore';
 
 export default function drawLines(talentIds: number[]) {
-  const { getById } = useTalentStore()
+  const { getById, getByName } = useTalentStore();
 
   for (const talentId of talentIds) {
-    const talent = getById(talentId)
-    if (talent.parentTalentId) {
-      arrowLine(toId(talent.parentTalentId), toId(talent.id))
+    const talent = getById(talentId);
+    if (talent.parentTalentName) {
+      const parentTalentId = getByName(talent.parentTalentName).id;
+      arrowLine(toId(parentTalentId), toId(talent.id));
     }
   }
 }
 
 function toId(value: number) {
-  return `#talent_${value}`
+  return `#talent_${value}`;
 }
