2753441a711b0f694d5146b1b62ccc85684c79b7
nicefsf <howyoumetmyriven@gmail.com>
2024-05-05 10:56:58 +0300

    Added image and centered items

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,18 @@ import ClassTalents from './components/ClassTalents.vue';
 </script>
 
 <template>
-  <ClassTalents :tree-ids="[1, 2, 3]" />
+  <div class="center">
+    <ClassTalents :tree-ids="[1, 2, 3]" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  overflow: hidden;
+  background-image: url('https://r4.wallpaperflare.com/wallpaper/260/652/937/world-of-warcraft-rogue-wallpaper-92d152f0bda6ae6baab8b295f0e82902.jpg');
+}
+</style>
